0123b40d9551eb53d714e7225725b8794e999e56
Your Name <you@your-domain.com>
2018-10-18 18:56:37 +0900

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by JihoonPark on 18/10/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
